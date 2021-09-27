ce7268c97223ca4ec6611f0c890bc7e58a89c49c
Siekierson <adrian.siekierko@gmail.com>
2021-09-27 22:55:44 +0200

    basic learning

--- js/units/learning.js
+++ js/units/learning.js
@@ -1,1 +1,24 @@
-export const learning = (config, unitNumber) => {};
+export const learning = (config, unitNumber) => {
+  const engH3 = document.querySelector(".eng");
+  const plH3 = document.querySelector(".pl");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  let progress = 0;
+  const loop = () => {
+    console.log(config[progress][1]);
+    console.log(progress);
+    engH3.textContent = config[progress][1];
+    plH3.textContent = config[progress][0];
+    prevButton.style.display = progress === 0 ? "none" : "block";
+  };
+  loop();
+  nextButton.addEventListener("click", () => {
+    progress += 1;
+    loop();
+  });
+  prevButton.addEventListener("click", () => {
+    progress -= 1;
+
+    loop();
+  });
+};
